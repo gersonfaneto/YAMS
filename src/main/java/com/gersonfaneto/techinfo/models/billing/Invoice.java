d57d30ac7ac6b50e5ba203cdd9e51ccae31ecd46
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-24 18:23:48 -0300

    feat(Invoice): Add class atributte "referenceID".

--- src/main/java/com/gersonfaneto/techinfo/models/billing/Invoice.java
+++ src/main/java/com/gersonfaneto/techinfo/models/billing/Invoice.java
@@ -3,19 +3,44 @@ package com.gersonfaneto.techinfo.models.billing;
 import java.util.List;
 
 public class Invoice {
-    private final Integer invoiceID;
-    private final Integer orderID;
+    private static int referenceID = 0;
+    private final int invoiceID;
+    private final int orderID;
+    private double totalValue;
+    private double payedValue;
     private List<Payment> receivedPayments;
-    private Double totalValue;
-    private Double payedValue;
 
-    public Invoice(Integer invoiceID, Integer orderID, Double totalValue) {
-        this.invoiceID = invoiceID;
+    public Invoice(int orderID, double totalValue) {
+        this.invoiceID = ++referenceID;
         this.orderID = orderID;
         this.totalValue = totalValue;
     }
 
-    public Payment newPayment() {
-        return null;
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public double getPayedValue() {
+        return payedValue;
+    }
+
+    public void setPayedValue(double payedValue) {
+        this.payedValue = payedValue;
+    }
+
+    public List<Payment> getReceivedPayments() {
+        return receivedPayments;
     }
 }
