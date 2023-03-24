0d92aa1473312bd982a7c9b1d5cc8a381553dd83
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-24 18:18:31 -0300

    feat(Order): Overloaded constructor to cover use cases.
    Now it is possible to create a "Order" with or without a preassigned
    "Technician".
    
    Also, added validation through some Setters.

--- src/main/java/com/gersonfaneto/techinfo/models/order/Order.java
+++ src/main/java/com/gersonfaneto/techinfo/models/order/Order.java
@@ -1,23 +1,32 @@
 package com.gersonfaneto.techinfo.models.order;
 
-import com.gersonfaneto.techinfo.models.billing.Invoice;
 import com.gersonfaneto.techinfo.models.service.Service;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Order {
-    private static Integer referenceID = 0;
-    private final Integer orderID;
-    private final Integer invoiceID;
-    private final Integer technicianID;
-    private final Integer clientID;
+    private static int referenceID = 0;
+    private final int orderID;
+    private final int invoiceID;
+    private final int clientID;
+    private int technicianID;
     private List<Service> registeredServices;
     private OrderStatus orderStatus;
     private Double averageRating;
 
-    public Order(Integer technicianID, Integer clientID) {
-        this.orderID = ++Order.referenceID;
+    public Order(int technicianID, int clientID) {
+        this.orderID = ++referenceID;
+        this.invoiceID = -1;
+        this.technicianID = technicianID;
+        this.clientID = clientID;
+        this.registeredServices = new LinkedList<>();
+        this.orderStatus = OrderStatus.WIP;
+        this.averageRating = 0.0;
+    }
+
+    public Order(int clientID) {
+        this.orderID = ++referenceID;
         this.invoiceID = -1;
         this.technicianID = -1;
         this.clientID = clientID;
@@ -26,15 +35,47 @@ public class Order {
         this.averageRating = 0.0;
     }
 
-    public Invoice generateInvoice() {
-        return null;
+    public int getOrderID() {
+        return orderID;
     }
 
-    public Service addService() {
-        return null;
+    public int getInvoiceID() {
+        return invoiceID;
     }
 
-    public Boolean removeService() {
-        return null;
+    public int getClientID() {
+        return clientID;
+    }
+
+    public int getTechnicianID() {
+        return technicianID;
+    }
+
+    public boolean setTechnicianID(int technicianID) {
+        if (this.technicianID == -1) {
+            this.technicianID = technicianID;
+            return true;
+        }
+        return false;
+    }
+
+    public List<Service> getRegisteredServices() {
+        return registeredServices;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
     }
 }
