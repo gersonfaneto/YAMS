ee8289a0d10da292488369851cb2383ae848e970
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-24 18:39:45 -0300

    fix(Client): List not initialized in constructor.

--- src/main/java/com/gersonfaneto/techinfo/models/Client.java
+++ src/main/java/com/gersonfaneto/techinfo/models/Client.java
@@ -2,6 +2,7 @@ package com.gersonfaneto.techinfo.models;
 
 import com.gersonfaneto.techinfo.models.order.Order;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Client {
@@ -13,9 +14,42 @@ public class Client {
     private List<Order> orderHistory;
 
     public Client(String clientName, String homeAddress, String phoneNumber) {
-        this.clientID = ++Client.referenceID;
+        this.clientID = ++referenceID;
         this.clientName = clientName;
         this.homeAddress = homeAddress;
         this.phoneNumber = phoneNumber;
+        this.orderHistory = new LinkedList<>();
+    }
+
+    public Integer getClientID() {
+        return clientID;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Order> getOrderHistory() {
+        return orderHistory;
     }
 }
