f08406ae4704f857c3967dc31ad854608bc84818
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-24 17:54:56 -0300

    feat(PurchaseOrder): Add ID attribute && Move to primitive types.

--- src/main/java/com/gersonfaneto/techinfo/models/stock/PurchaseOrder.java
+++ src/main/java/com/gersonfaneto/techinfo/models/stock/PurchaseOrder.java
@@ -1,13 +1,40 @@
 package com.gersonfaneto.techinfo.models.stock;
 
 public class PurchaseOrder {
+    private static int referenceID = 0;
     private ComponentType purchasedComponentType;
-    private Integer purchasedAmount;
-    private Double unitaryValue;
+    private int purchaseOrderID;
+    private int purchasedAmount;
+    private double unitaryValue;
 
-    public PurchaseOrder(ComponentType purchasedComponentType, Integer purchasedAmount, Double unitaryValue) {
+    public PurchaseOrder(ComponentType purchasedComponentType, int purchasedAmount, double unitaryValue) {
+        this.purchaseOrderID = ++referenceID;
         this.purchasedComponentType = purchasedComponentType;
         this.purchasedAmount = purchasedAmount;
+        this.unitaryValue = unitaryValue;
+    }
+
+    public ComponentType getPurchasedComponentType() {
+        return purchasedComponentType;
+    }
+
+    public void setPurchasedComponentType(ComponentType purchasedComponentType) {
+        this.purchasedComponentType = purchasedComponentType;
+    }
+
+    public int getPurchasedAmount() {
+        return purchasedAmount;
+    }
+
+    public void setPurchasedAmount(int purchasedAmount) {
+        this.purchasedAmount = purchasedAmount;
+    }
+
+    public double getUnitaryValue() {
+        return unitaryValue;
+    }
+
+    public void setUnitaryValue(double unitaryValue) {
         this.unitaryValue = unitaryValue;
     }
 }
