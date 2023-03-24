9f122f2421daffb0910010e99ad3f10daf5c5f18
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-24 18:08:24 -0300

    feat(Service): Make some attributes final && Move to primitive types.
    "orderID" and "openingDate" were made final as there doesn't seem to be
    a reason for them to change.
    Also, add the "referenceID" attribute to the class and use it for
    "orderID" under the constructor.

--- src/main/java/com/gersonfaneto/techinfo/models/service/Service.java
+++ src/main/java/com/gersonfaneto/techinfo/models/service/Service.java
@@ -5,39 +5,72 @@ import com.gersonfaneto.techinfo.models.stock.Component;
 import java.util.Date;
 
 public class Service {
+    private static int referenceID = 0;
     private final ServiceType serviceType;
-    private final Integer orderID;
-    private Date openingDate;
+    private final int orderID;
+    private final Date openingDate;
     private Date closingDate;
-    private Double serviceCost;
+    private double serviceCost;
     private Component usedComponent;
-    private Double clientRating;
+    private double clientRating;
 
-    public Service(ServiceType serviceType, Date openingDate, Integer orderID, Component usedComponent) {
+    public Service(ServiceType serviceType, Date openingDate, Component usedComponent, double serviceCost) {
         this.serviceType = serviceType;
-        this.orderID = orderID;
+        this.orderID = ++referenceID;
         this.openingDate = openingDate;
         this.closingDate = null;
         this.usedComponent = usedComponent;
         this.clientRating = 0.0;
-        this.serviceCost = 0.0;
+        this.serviceCost = serviceCost;
     }
 
-    public Service(ServiceType serviceType, Integer orderID, Date openingDate) {
+    public Service(ServiceType serviceType, Date openingDate, double serviceCost) {
         this.serviceType = serviceType;
-        this.orderID = orderID;
+        this.orderID = ++referenceID;
         this.openingDate = openingDate;
         this.closingDate = null;
         this.usedComponent = null;
         this.clientRating = 0.0;
-        this.serviceCost = 0.0;
+        this.serviceCost = serviceCost;
     }
 
-    public Component addComponent() {
-        return null;
+    public ServiceType getServiceType() {
+        return serviceType;
     }
 
-    public Boolean removeComponent() {
-        return true;
+    public Date getOpeningDate() {
+        return openingDate;
+    }
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public double getServiceCost() {
+        return serviceCost;
+    }
+
+    public void setServiceCost(double serviceCost) {
+        this.serviceCost = serviceCost;
+    }
+
+    public Component getUsedComponent() {
+        return usedComponent;
+    }
+
+    public void setUsedComponent(Component usedComponent) {
+        this.usedComponent = usedComponent;
+    }
+
+    public double getClientRating() {
+        return clientRating;
+    }
+
+    public void setClientRating(double clientRating) {
+        this.clientRating = clientRating;
     }
 }
