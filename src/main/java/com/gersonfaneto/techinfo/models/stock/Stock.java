fa6445715d3d0671a62f595718fe22d7e0d7d556
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-29 06:00:09 -0300

    feat(Stock): Add base methods implementations.
    `Stock` will be used to create and process `PurchaseOrder` objects.
    It will also be responsible for checking the availability of the
    `Component`s through the `DAO`.

--- src/main/java/com/gersonfaneto/techinfo/models/stock/Stock.java
+++ src/main/java/com/gersonfaneto/techinfo/models/stock/Stock.java
@@ -1,14 +1,13 @@
 package com.gersonfaneto.techinfo.models.stock;
 
+import com.gersonfaneto.techinfo.dao.DAO;
 import com.gersonfaneto.techinfo.models.stock.component.Component;
+import com.gersonfaneto.techinfo.models.stock.component.ComponentType;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class Stock {
     private static Stock singleInstance;
-    private static List<Component> availableComponents;
-    private static List<PurchaseOrder> purchaseOrderHistory;
 
     private Stock() {
     }
@@ -16,9 +15,45 @@ public class Stock {
     public static Stock retrieveStock() {
         if (singleInstance == null) {
             singleInstance = new Stock();
-            availableComponents = new LinkedList<>();
-            purchaseOrderHistory = new LinkedList<>();
         }
         return singleInstance;
     }
+
+    public boolean hasComponent(ComponentType targetType) {
+        return DAO.fromComponents().findByType(targetType).size() > 0;
+    }
+
+    public List<Component> retrieveAvailableComponents() {
+        return DAO.fromComponents().findMany();
+    }
+
+    public List<Component> retrieveAvailableComponents(ComponentType targetType) {
+        return DAO.fromComponents().findByType(targetType);
+    }
+
+    public PurchaseOrder makePurchase(String technicianID, ComponentType componentType, String componentDescription, int boughtAmount, double unitaryPrice, double unitaryCost) {
+        PurchaseOrder newPurchaseOrder = new PurchaseOrder(technicianID, componentType, componentDescription, boughtAmount, unitaryPrice, unitaryCost);
+
+        return DAO.fromPurchaseOrders().createOne(newPurchaseOrder);
+    }
+
+    public PurchaseOrder makePurchase(String technicianID, ComponentType componentType, String componentDescription, int boughtAmount, double unitaryCost) {
+        PurchaseOrder newPurchaseOrder = new PurchaseOrder(technicianID, componentType, componentDescription, boughtAmount, unitaryCost);
+
+        return DAO.fromPurchaseOrders().createOne(newPurchaseOrder);
+    }
+
+    public void arrangeComponents(PurchaseOrder purchaseOrder) {
+        for (int i = 0; i < purchaseOrder.getPurchasedAmount(); i++) {
+            DAO.fromComponents().createOne(
+                    new Component(
+                            purchaseOrder.getComponentType(),
+                            purchaseOrder.getComponentDescription(),
+                            purchaseOrder.getUnitaryPrice(),
+                            purchaseOrder.getUnitaryCost()
+                    )
+            );
+        }
+    }
+
 }
