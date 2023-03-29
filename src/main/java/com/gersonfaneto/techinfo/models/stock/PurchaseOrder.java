57c9a4a54eb6d5a1244d2774a1bf335ac4921bc5
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-29 05:16:23 -0300

    feat(PurchaseOrder): Change ID type to `String`.

--- src/main/java/com/gersonfaneto/techinfo/models/stock/PurchaseOrder.java
+++ src/main/java/com/gersonfaneto/techinfo/models/stock/PurchaseOrder.java
@@ -1,56 +1,86 @@
 package com.gersonfaneto.techinfo.models.stock;
 
-public class PurchaseOrder {
-    private ComponentType purchasedComponentType;
-    private int purchaseOrderID;
-    private int technicianID;
-    private int purchasedAmount;
-    private double unitaryValue;
+import com.gersonfaneto.techinfo.models.stock.component.ComponentType;
 
-    public PurchaseOrder(ComponentType purchasedComponentType, int technicianID, int purchasedAmount, double unitaryValue) {
-        this.purchasedComponentType = purchasedComponentType;
+public class PurchaseOrder {
+    private final String technicianID;
+    private final ComponentType componentType;
+    private final String componentDescription;
+    private final int chosenAmount;
+    private final double unitaryPrice;
+    private final double unitaryCost;
+    private String purchaseOrderID;
+
+    public PurchaseOrder(String technicianID, ComponentType componentType, String componentDescription, int chosenAmount, double unitaryPrice, double unitaryCost) {
+        this.purchaseOrderID = "Undefined!";
         this.technicianID = technicianID;
-        this.purchasedAmount = purchasedAmount;
-        this.unitaryValue = unitaryValue;
+        this.componentType = componentType;
+        this.componentDescription = componentDescription;
+        this.chosenAmount = chosenAmount;
+        this.unitaryPrice = unitaryPrice;
+        this.unitaryCost = unitaryCost;
     }
 
-    public int getPurchaseOrderID() {
+    public PurchaseOrder(String technicianID, ComponentType componentType, String componentDescription, int chosenAmount, double unitaryCost) {
+        this.purchaseOrderID = "Undefined!";
+        this.technicianID = technicianID;
+        this.componentType = componentType;
+        this.componentDescription = componentDescription;
+        this.chosenAmount = chosenAmount;
+        this.unitaryPrice = componentType.getTypeValue();
+        this.unitaryCost = unitaryCost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                ID: %s
+                Technician: %s
+                Component: %s
+                Description: %s
+                Price: R$ %.2f
+                Cost: R$: %.2f
+                """, purchaseOrderID, technicianID, componentType.getTypeName(), componentDescription, unitaryPrice, unitaryCost);
+    }
+
+    @Override
+    public boolean equals(Object objectToCompare) {
+        if (objectToCompare instanceof PurchaseOrder otherPurchaseOrder) {
+            return otherPurchaseOrder.purchaseOrderID.equals(this.purchaseOrderID);
+        }
+
+        return false;
+    }
+
+    public String getPurchaseOrderID() {
         return purchaseOrderID;
     }
 
-    public void setPurchaseOrderID(int purchaseOrderID) {
+    public void setPurchaseOrderID(String purchaseOrderID) {
         this.purchaseOrderID = purchaseOrderID;
     }
 
-    public int getTechnicianID() {
+    public String getTechnicianID() {
         return technicianID;
     }
 
-    public void setTechnicianID(int technicianID) {
-        this.technicianID = technicianID;
+    public ComponentType getComponentType() {
+        return componentType;
     }
 
-    public ComponentType getPurchasedComponentType() {
-        return purchasedComponentType;
+    public String getComponentDescription() {
+        return componentDescription;
     }
 
-    public void setPurchasedComponentType(ComponentType purchasedComponentType) {
-        this.purchasedComponentType = purchasedComponentType;
+    public int getChosenAmount() {
+        return chosenAmount;
     }
 
-    public int getPurchasedAmount() {
-        return purchasedAmount;
+    public double getUnitaryPrice() {
+        return unitaryPrice;
     }
 
-    public void setPurchasedAmount(int purchasedAmount) {
-        this.purchasedAmount = purchasedAmount;
-    }
-
-    public double getUnitaryValue() {
-        return unitaryValue;
-    }
-
-    public void setUnitaryValue(double unitaryValue) {
-        this.unitaryValue = unitaryValue;
+    public double getUnitaryCost() {
+        return unitaryCost;
     }
 }
