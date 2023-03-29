1ba22f01816ff514ee69d6e68550ee8d8709d9eb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-29 05:34:28 -0300

    feat(Technician): Override `toString` and `equals` && Fix typo.

--- src/main/java/com/gersonfaneto/techinfo/models/entities/Technician.java
+++ src/main/java/com/gersonfaneto/techinfo/models/entities/Technician.java
@@ -4,7 +4,7 @@ import com.gersonfaneto.techinfo.models.entities.user.User;
 import com.gersonfaneto.techinfo.models.entities.user.UserType;
 
 public class Technician extends User {
-    private final boolean hasOrderOpen;
+    private boolean hasOrderOpen;
     private String technicianName;
 
     public Technician(String userEmail, String userPassword, UserType userType, String technicianName) {
@@ -13,11 +13,40 @@ public class Technician extends User {
         this.hasOrderOpen = false;
     }
 
+    @Override
+    public String toString() {
+        return String.format("""
+                ID: %s
+                Name: %s
+                Email: %s
+                Password: %s
+                User Type: %s
+                """, super.getUserID(), technicianName, super.getUserEmail(), super.getUserPassword(), super.getUserType().getTypeName());
+    }
+
+    @Override
+    public boolean equals(Object objectToCompare) {
+        if (objectToCompare instanceof Technician) {
+            Technician otherTechnician = (Technician) objectToCompare;
+            return otherTechnician.getUserID().equals(this.getUserID());
+        }
+
+        return false;
+    }
+
     public String getTechnicianName() {
         return technicianName;
     }
 
     public void setTechnicianName(String technicianName) {
         this.technicianName = technicianName;
+    }
+
+    public boolean isHasOrderOpen() {
+        return hasOrderOpen;
+    }
+
+    public void setHasOrderOpen(boolean hasOrderOpen) {
+        this.hasOrderOpen = hasOrderOpen;
     }
 }
