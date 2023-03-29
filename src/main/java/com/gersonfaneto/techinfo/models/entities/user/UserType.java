150e3256bdb6ab70e808361a99c8b4a3ba069fd7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-29 05:09:49 -0300

    feat: Add abstract class `User`.

---
+++ src/main/java/com/gersonfaneto/techinfo/models/entities/user/UserType.java
@@ -0,0 +1,21 @@
+package com.gersonfaneto.techinfo.models.entities.user;
+
+public enum UserType {
+    Normal("Normal"),
+    Administrator("Administrator");
+
+    private final String typeName;
+
+    UserType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s", typeName);
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+}
