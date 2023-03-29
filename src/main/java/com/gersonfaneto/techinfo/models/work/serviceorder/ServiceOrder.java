b398798c1713e046d8da15976d0a4e2251a4aead
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-29 08:07:17 -0300

    feat(ServiceOrder): Add missing methods.

--- src/main/java/com/gersonfaneto/techinfo/models/work/serviceorder/ServiceOrder.java
+++ src/main/java/com/gersonfaneto/techinfo/models/work/serviceorder/ServiceOrder.java
@@ -1,11 +1,17 @@
 package com.gersonfaneto.techinfo.models.work.serviceorder;
 
+import com.gersonfaneto.techinfo.dao.DAO;
+import com.gersonfaneto.techinfo.models.billing.Invoice;
+import com.gersonfaneto.techinfo.models.stock.component.Component;
 import com.gersonfaneto.techinfo.models.work.service.Service;
+import com.gersonfaneto.techinfo.models.work.service.ServiceType;
 
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.SplittableRandom;
 
-import static com.gersonfaneto.techinfo.models.work.serviceorder.ServiceOrderStatus.Created;
+import static com.gersonfaneto.techinfo.models.work.serviceorder.ServiceOrderStatus.*;
 
 public class ServiceOrder {
     private final String clientID;
@@ -36,6 +42,65 @@ public class ServiceOrder {
                 Opened At: %s
                 Status: %s
                 """, serviceOrderID, clientID, openedAt.toString(), serviceOrderStatus.getTypeName());
+    }
+
+    public double calculatePrice() {
+        List<Service> providedServices = DAO.fromServices().findByOrderID(this.serviceOrderID);
+        double totalPrice = 0.0;
+
+        for (Service currentService : providedServices) {
+            totalPrice += currentService.getServicePrice();
+        }
+
+        return totalPrice;
+    }
+
+    public boolean generateInvoice() {
+        if (this.serviceOrderStatus != Finished) {
+            return false;
+        }
+
+        Invoice newInvoice = new Invoice(this.serviceOrderID, calculatePrice());
+        DAO.fromInvoices().createOne(newInvoice);
+
+        this.invoiceID = newInvoice.getInvoiceID();
+        DAO.fromServiceOrders().updateInformation(this);
+
+        return true;
+    }
+
+    public boolean addService(String technicianID, ServiceType serviceType, String serviceDescription, Component usedComponent) {
+        if (this.serviceOrderStatus != Open || technicianID != this.technicianID) {
+            return false;
+        }
+
+        Service newService = new Service(this.serviceOrderID, serviceType, serviceDescription, usedComponent);
+
+        DAO.fromServices().createOne(newService);
+
+        return true;
+    }
+
+    public boolean addService(String technicianID, String orderID, ServiceType serviceType, String serviceDescription) {
+        if (this.serviceOrderStatus != Open || technicianID != this.technicianID) {
+            return false;
+        }
+
+        Service newService = new Service(this.serviceOrderID, serviceType, serviceDescription);
+
+        DAO.fromServices().createOne(newService);
+
+        return true;
+    }
+
+    public boolean removeService(String technicianID, String serviceID) {
+        if (this.serviceOrderStatus != Open || this.technicianID != technicianID) {
+            return false;
+        }
+
+        DAO.fromServices().deleteByID(serviceID);
+
+        return true;
     }
 
     @Override
