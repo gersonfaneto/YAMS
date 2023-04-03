5cd09bb5c48e82bc2793caaf9957f19732b8b3dc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-03 11:10:54 -0300

    chore!: Back to the drawing board...
    I've gotten a bit confuse with the flow of the project...
    So I've decide to restart from the beginning 🙃.

--- src/main/java/com/gersonfaneto/yams/dao/client/ClientDAO.java
+++
@@ -1,8 +0,0 @@
-package com.gersonfaneto.yams.dao.client;
-
-import com.gersonfaneto.yams.dao.CRUD;
-import com.gersonfaneto.yams.models.entities.Client;
-
-public interface ClientDAO extends CRUD<Client> {
-    Client findByName(String clientName);
-}
