cc99646a034a352a2fcc4e281c93673a45fbf1f1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-03 12:58:52 -0300

    feat(components): Add `types`.
    Each component type know is implemented extending the super class
    `component`.

---
+++ src/main/java/com/gersonfaneto/yams/models/components/types/GraphicsCard.java
@@ -0,0 +1,12 @@
+package com.gersonfaneto.yams.models.components.types;
+
+import com.gersonfaneto.yams.models.components.Component;
+
+public class GraphicsCard extends Component {
+    private static final String COMPONENT_TYPE = "Graphics Card";
+    private static final double COMPONENT_PRICE = 100.00;
+
+    public GraphicsCard(String componentDescription, double componentCost) {
+        super(COMPONENT_TYPE, componentDescription, componentCost, COMPONENT_PRICE);
+    }
+}
