74b6833b3a8eadde313c1a92746d7d6913bde675
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-03 22:05:27 -0300

    feat(DAO): Add method to access `Client` DAO.

--- src/main/java/com/gersonfaneto/yams/dao/DAO.java
+++ src/main/java/com/gersonfaneto/yams/dao/DAO.java
@@ -1,4 +1,16 @@
 package com.gersonfaneto.yams.dao;
 
-public class DAO {
+import com.gersonfaneto.yams.dao.client.ClientCRUD;
+import com.gersonfaneto.yams.dao.client.ClientMemoryDAO;
+
+public abstract class DAO {
+    private static ClientCRUD clientCRUD;
+
+    public static ClientCRUD fromClients() {
+        if (clientCRUD == null) {
+            clientCRUD = new ClientMemoryDAO();
+        }
+
+        return clientCRUD;
+    }
 }
