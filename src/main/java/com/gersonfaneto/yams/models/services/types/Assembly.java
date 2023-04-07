7ed6ef3b78220e4642cb8d322fafc44dd01669c1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-07 14:38:13 -0300

    feat(Assembly): Add missing implementations.

--- src/main/java/com/gersonfaneto/yams/models/services/types/Assembly.java
+++ src/main/java/com/gersonfaneto/yams/models/services/types/Assembly.java
@@ -1,5 +1,6 @@
 package com.gersonfaneto.yams.models.services.types;
 
+import com.gersonfaneto.yams.dao.DAO;
 import com.gersonfaneto.yams.models.components.Component;
 import com.gersonfaneto.yams.models.services.Service;
 import java.util.LinkedList;
@@ -25,12 +26,37 @@ public class Assembly extends Service {
     return new Assembly(workOrderID, serviceDescription);
   }
 
-  // TODO: Add Implementations!
   public boolean addComponent(String componentID) {
+    Component chosenComponent = DAO.fromComponents().findByID(componentID);
+
+    if (chosenComponent == null) {
+      return false;
+    }
+
+    DAO.fromComponents().deleteByID(componentID);
+
+    usedComponents.add(chosenComponent);
+
+    setServicePrice(getServicePrice() + chosenComponent.getComponentPrice());
+
     return true;
   }
 
   public boolean removeComponent(String componentID) {
+    Component chosenComponent = usedComponents.stream()
+        .filter(x -> x.getComponentID().equals(componentID))
+        .findFirst()
+        .orElse(null);
+
+    if (chosenComponent == null) {
+      return false;
+    }
+
+    usedComponents.remove(chosenComponent);
+    setServicePrice(getServicePrice() - chosenComponent.getComponentPrice());
+
+    DAO.fromComponents().createOne(chosenComponent);
+
     return true;
   }
 
