14ad0fd47293e2fab955c02ac0e696d1ca92d1b9
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-07 13:25:01 -0300

    feat(factories): Add `PaymentFactory`.
    Used to create new instances of `Payment` based on Prototype and
    Abstract Factory Design Patterns.

---
+++ src/main/java/com/gersonfaneto/yams/factories/types/PaymentFactory.java
@@ -0,0 +1,29 @@
+package com.gersonfaneto.yams.factories.types;
+
+import com.gersonfaneto.yams.models.billing.payments.Payment;
+import com.gersonfaneto.yams.models.billing.payments.types.Cash;
+import com.gersonfaneto.yams.models.billing.payments.types.CreditCard;
+import com.gersonfaneto.yams.models.billing.payments.types.Transference;
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentFactory {
+    private final Map<String, Payment> paymentPrototypes;
+
+    public PaymentFactory() {
+      this.paymentPrototypes = new HashMap<>();
+
+      paymentPrototypes.put("Cash", new Cash());
+      paymentPrototypes.put("Transference", new Transference());
+      paymentPrototypes.put("Credit Card", new CreditCard());
+    }
+
+    public Payment generatePayment(String paymentMethod, String invoiceID, double paidValue) {
+      if (!paymentPrototypes.containsKey(paymentMethod)) {
+        throw new InvalidParameterException("Payment method not found!");
+      }
+
+      return paymentPrototypes.get(paymentMethod).clone(invoiceID, paidValue);
+    }
+}
