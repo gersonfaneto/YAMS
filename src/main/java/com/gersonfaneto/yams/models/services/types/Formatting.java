dd7c55fed9ced80b6bb4cc26cf2df9124b6cab3a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-07 17:02:44 -0300

    feat(Formatting): Add attribute `chosenOS`.

--- src/main/java/com/gersonfaneto/yams/models/services/types/Formatting.java
+++ src/main/java/com/gersonfaneto/yams/models/services/types/Formatting.java
@@ -5,6 +5,7 @@ import com.gersonfaneto.yams.models.services.Service;
 public class Formatting extends Service {
   private static final String SERVICE_TYPE = "Formatting";
   private static final double BASE_PRICE = 50.00;
+  private String chosenOS;
 
   public Formatting(String workOrderID, String serviceDescription) {
     super(workOrderID, SERVICE_TYPE, serviceDescription, BASE_PRICE);
@@ -17,5 +18,13 @@ public class Formatting extends Service {
   @Override
   public Formatting clone(String workOrderID, String serviceDescription) {
     return new Formatting(workOrderID, serviceDescription);
+  }
+
+  public String getChosenOS() {
+    return chosenOS;
+  }
+
+  public void setChosenOS(String chosenOS) {
+    this.chosenOS = chosenOS;
   }
 }
