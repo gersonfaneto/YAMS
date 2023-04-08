24c75512757b0dde85be984fbc7c315301a803a0
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-08 18:47:50 -0300

    feat(builders): Add `PurchaseOrder` builder.

---
+++ src/main/java/com/gersonfaneto/yams/builders/orders/purchase/PurchaseOrderBuilder.java
@@ -0,0 +1,69 @@
+package com.gersonfaneto.yams.builders.orders.purchase;
+
+import static com.gersonfaneto.yams.models.components.ComponentType.Others;
+
+import com.gersonfaneto.yams.builders.Builder;
+import com.gersonfaneto.yams.models.components.ComponentType;
+import com.gersonfaneto.yams.models.orders.purchase.PurchaseOrder;
+import java.security.InvalidParameterException;
+
+public class PurchaseOrderBuilder implements Builder<PurchaseOrder> {
+  private final ComponentType componentType;
+  private String componentDescription;
+  private int boughtAmount;
+  private double componentCost;
+  private double componentPrice;
+
+  public PurchaseOrderBuilder(String componentType) {
+    if (ComponentType.findByType(componentType) == null) {
+      throw new InvalidParameterException("Component type not known!");
+    }
+    this.componentType = ComponentType.findByType(componentType);
+  }
+
+
+  public PurchaseOrderBuilder componentDescription(String componentDescription) {
+    this.componentDescription = componentDescription;
+    return this;
+  }
+
+  public PurchaseOrderBuilder boughtAmount(int boughtAmount) {
+    this.boughtAmount = boughtAmount;
+    return this;
+  }
+
+  public PurchaseOrderBuilder costOfEach(double componentCost) {
+    this.componentCost = componentCost;
+    return this;
+  }
+
+  public PurchaseOrderBuilder priceOfEach(double componentPrice) {
+    this.componentPrice = (componentType == Others) ? componentPrice : componentType.getTypeValue();
+    return this;
+  }
+
+  @Override
+  public PurchaseOrder Build() {
+    return new PurchaseOrder(this);
+  }
+
+  public ComponentType getComponentType() {
+    return componentType;
+  }
+
+  public String getComponentDescription() {
+    return componentDescription;
+  }
+
+  public int getBoughtAmount() {
+    return boughtAmount;
+  }
+
+  public double getComponentCost() {
+    return componentCost;
+  }
+
+  public double getComponentPrice() {
+    return componentPrice;
+  }
+}
