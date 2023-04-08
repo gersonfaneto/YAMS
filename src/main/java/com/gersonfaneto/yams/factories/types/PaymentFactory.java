a50b403493c1f9fa523b6f453d0451a2907bbe14
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-08 14:38:58 -0300

    feat(types): Add new payment method `DebitCard`.

--- src/main/java/com/gersonfaneto/yams/factories/types/PaymentFactory.java
+++ src/main/java/com/gersonfaneto/yams/factories/types/PaymentFactory.java
@@ -3,6 +3,7 @@ package com.gersonfaneto.yams.factories.types;
 import com.gersonfaneto.yams.models.billing.payments.Payment;
 import com.gersonfaneto.yams.models.billing.payments.types.Cash;
 import com.gersonfaneto.yams.models.billing.payments.types.CreditCard;
+import com.gersonfaneto.yams.models.billing.payments.types.DebitCard;
 import com.gersonfaneto.yams.models.billing.payments.types.Transference;
 import java.security.InvalidParameterException;
 import java.util.HashMap;
@@ -17,6 +18,7 @@ public class PaymentFactory {
       paymentPrototypes.put("Cash", new Cash());
       paymentPrototypes.put("Transference", new Transference());
       paymentPrototypes.put("Credit Card", new CreditCard());
+      paymentPrototypes.put("Debit Card", new DebitCard());
     }
 
     public Payment generatePayment(String paymentMethod, String invoiceID, double paidValue) {
