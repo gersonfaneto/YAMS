4e6a90985dee8f8b08f90eec88b6425a20a5c927
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-09 18:21:31 -0300

    feat(exceptions): Add exceptions related to `Service` and `WorkOrder`.

---
+++ src/main/java/com/gersonfaneto/yams/exceptions/UnsupportedOperationException.java
@@ -0,0 +1,8 @@
+package com.gersonfaneto.yams.exceptions;
+
+public class UnsupportedOperationException extends Exception {
+
+  public UnsupportedOperationException(String errorMessage) {
+    super(errorMessage);
+  }
+}
