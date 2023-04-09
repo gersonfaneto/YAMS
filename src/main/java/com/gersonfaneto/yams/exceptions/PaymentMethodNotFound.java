8c383d568c3bfe8a3f97077f281220a2454e5f3c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-09 15:13:30 -0300

    feat(exceptions): Add `Payment` and `Invoice` related exceptions.

---
+++ src/main/java/com/gersonfaneto/yams/exceptions/PaymentMethodNotFound.java
@@ -0,0 +1,8 @@
+package com.gersonfaneto.yams.exceptions;
+
+public class PaymentMethodNotFound extends Exception {
+
+  public PaymentMethodNotFound(String errorMessage) {
+    super(errorMessage);
+  }
+}
---
+++ src/main/java/com/gersonfaneto/yams/exceptions/WorkOrderNotFound.java
@@ -0,0 +1,8 @@
+package com.gersonfaneto.yams.exceptions;
+
+public class WorkOrderNotFound extends Exception {
+
+  public WorkOrderNotFound(String errorMessage) {
+    super(errorMessage);
+  }
+}
