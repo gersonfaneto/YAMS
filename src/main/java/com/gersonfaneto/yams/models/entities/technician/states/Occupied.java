a465e8442fc5697f24ceafbd2e603bd01e8d7c3c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-16 08:55:57 -0300

    feat(Occupied): Add checking for completed `Service`s on methods.

--- src/main/java/com/gersonfaneto/yams/models/entities/technician/states/Occupied.java
+++ src/main/java/com/gersonfaneto/yams/models/entities/technician/states/Occupied.java
@@ -1,11 +1,15 @@
 package com.gersonfaneto.yams.models.entities.technician.states;
 
+import com.gersonfaneto.yams.dao.DAO;
+import com.gersonfaneto.yams.exceptions.orders.UnsupportedOperationException;
 import com.gersonfaneto.yams.models.billing.invoice.Invoice;
 import com.gersonfaneto.yams.models.entities.technician.Technician;
 import com.gersonfaneto.yams.models.orders.work.WorkOrder;
 import com.gersonfaneto.yams.models.orders.work.states.Canceled;
 import com.gersonfaneto.yams.models.orders.work.states.Finished;
+import com.gersonfaneto.yams.models.services.Service;
 import java.util.Calendar;
+import java.util.List;
 
 public class Occupied extends State {
 
@@ -20,15 +24,33 @@ public class Occupied extends State {
 
   @Override
   public boolean cancelOrder() {
+    List<Service> relatedServices = DAO.fromService()
+        .findByWorkOrder(getWorkOrder().getWorkOrderID());
+
+    for (Service currentService : relatedServices) {
+      if (!currentService.isComplete()) {
+        return false;
+      }
+    }
+
     getWorkOrder().setClosedAt(Calendar.getInstance());
     getWorkOrder().setWorkOrderState(new Canceled(getWorkOrder()));
     getTechnician().setTechnicianState(new Free(getTechnician()));
 
-    return false;
+    return true;
   }
 
   @Override
   public boolean closeOrder() {
+    List<Service> relatedServices = DAO.fromService()
+        .findByWorkOrder(getWorkOrder().getWorkOrderID());
+
+    for (Service currentService : relatedServices) {
+      if (!currentService.isComplete()) {
+        return false;
+      }
+    }
+
     getWorkOrder().setClosedAt(Calendar.getInstance());
     getWorkOrder().setWorkOrderState(new Finished(getWorkOrder()));
     getTechnician().setTechnicianState(new Free(getTechnician()));
