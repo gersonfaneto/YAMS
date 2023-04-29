91b751a3fdddcbc6d2cefa2670cb6cbdef9b0297
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-29 18:45:37 -0300

    docs(ObjectIO): Add JavaDoc.

--- src/main/java/com/gersonfaneto/yams/utils/ObjectIO.java
+++ src/main/java/com/gersonfaneto/yams/utils/ObjectIO.java
@@ -6,26 +6,52 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * <code>ObjectIO</code> contains functions for dealing with: reading and writing data off/from
+ * <code>Serializable</code> Objects.
+ *
+ * @author gersonfaneto
+ * @version 1.0.0
+ * @see java.io.Serializable
+ */
 public abstract class ObjectIO {
-    public static void savePerson(Object targetPerson, String savePath) throws IOException {
-        FileOutputStream fileOut = new FileOutputStream(savePath);
-        ObjectOutputStream objectOutput = new ObjectOutputStream(fileOut);
 
-        objectOutput.writeObject(targetPerson);
+  /**
+   * Saves a received <code>Object</code> data in to a file.
+   *
+   * @param targetObject The <code>Object</code> to be saved.
+   * @param savePath     The path of the new file.
+   * @throws IOException If any file I/O error happens in the process.
+   */
+  public static void savePerson(Object targetObject, String savePath) throws IOException {
+    FileOutputStream fileOut = new FileOutputStream(savePath);
+    ObjectOutputStream objectOutput = new ObjectOutputStream(fileOut);
 
-        fileOut.close();
-        objectOutput.close();
-    }
+    objectOutput.writeObject(targetObject);
 
-    public static Object loadPerson(String savePath) throws IOException, ClassNotFoundException {
-        FileInputStream fileIn = new FileInputStream(savePath);
-        ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+    fileOut.close();
+    objectOutput.close();
+  }
 
-        Object readObject = objectIn.readObject();
+  /**
+   * Reads the data from the received file path and attempts to reconstruct an <code>Object</code>
+   * from it.
+   *
+   * @param savePath The path of the file to be read.
+   * @return The retrieved <code>Object</code>.
+   * @throws IOException            If any file I/O error happens in the process.
+   * @throws ClassNotFoundException If the <code>savePath</code> data didn't match any of the known
+   *                                <code>Serializable</code> Objects.
+   */
+  public static Object loadPerson(String savePath) throws IOException, ClassNotFoundException {
+    FileInputStream fileIn = new FileInputStream(savePath);
+    ObjectInputStream objectIn = new ObjectInputStream(fileIn);
 
-        fileIn.close();
-        objectIn.close();
+    Object readObject = objectIn.readObject();
 
-        return readObject;
-    }
+    fileIn.close();
+    objectIn.close();
+
+    return readObject;
+  }
 }
