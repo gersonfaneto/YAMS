876b625536a2b2dceabaf06f59d592f3428b4ad9
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-30 19:40:25 -0300

    feat(ObjectIO): Updated methods and made into a concrete class.

--- src/main/java/com/gersonfaneto/yams/utils/ObjectIO.java
+++ src/main/java/com/gersonfaneto/yams/utils/ObjectIO.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * <code>ObjectIO</code> contains functions for dealing with: reading and writing data off/from
@@ -13,20 +15,32 @@ import java.io.ObjectOutputStream;
  * @version 1.0.0
  * @see java.io.Serializable
  */
-public abstract class ObjectIO {
+public class ObjectIO<T> {
+
+  private String savePath;
 
   /**
-   * Saves a received <code>Object</code> data in to a file.
+   * Constructs a new <code>ObjectIO</code>.
    *
-   * @param targetObject The <code>Object</code> to be saved.
-   * @param savePath     The path of the new file.
+   * @param savePath The file to save the objects in.
    */
-  public static boolean saveObject(Object targetObject, String savePath) {
+  public ObjectIO(String savePath) {
+    this.savePath = savePath;
+  }
+
+  /**
+   * Saves all the received <code>Object</code>s data in to a file.
+   *
+   * @param objectsToSave The <code>List</code> of <code>Object</code>s to be saved.
+   */
+  public boolean saveObjects(List<T> objectsToSave) {
     try {
       FileOutputStream fileOut = new FileOutputStream(savePath);
       ObjectOutputStream objectOutput = new ObjectOutputStream(fileOut);
 
-      objectOutput.writeObject(targetObject);
+      for (T currentObject : objectsToSave) {
+        objectOutput.writeObject(currentObject);
+      }
 
       fileOut.close();
       objectOutput.close();
@@ -41,22 +55,34 @@ public abstract class ObjectIO {
    * Reads the data from the received file path and attempts to reconstruct an <code>Object</code>
    * from it.
    *
-   * @param savePath The path of the file to be read.
-   * @return The retrieved <code>Object</code>.
+   * @return The <code>List</code> of all retrieved <code>Object</code>s.
    */
-  public static Object loadObject(String savePath) {
+  public List<T> loadObjects() {
     try {
+      List<T> retrievedObjects = new LinkedList<>();
+
       FileInputStream fileIn = new FileInputStream(savePath);
       ObjectInputStream objectIn = new ObjectInputStream(fileIn);
 
-      Object readObject = objectIn.readObject();
+      while (fileIn.available() > 0) {
+        T readObject = (T) objectIn.readObject();
+        retrievedObjects.add(readObject);
+      }
 
       fileIn.close();
       objectIn.close();
 
-      return readObject;
+      return retrievedObjects;
     } catch (Exception e) {
       return null;
     }
+  }
+
+  public String getSavePath() {
+    return savePath;
+  }
+
+  public void setSavePath(String savePath) {
+    this.savePath = savePath;
   }
 }
