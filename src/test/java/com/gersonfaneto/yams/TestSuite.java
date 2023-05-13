9630219f2c4eaa82475c53b9e3abc128ac461475
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-13 20:13:26 -0300

    test!: Added `TestSuite` for proper Unit Tests coverage.

---
+++ src/test/java/com/gersonfaneto/yams/TestSuite.java
@@ -0,0 +1,9 @@
+package com.gersonfaneto.yams;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@SelectPackages("com.gersonfaneto.yams.dao")
+@Suite
+public class TestSuite {
+}
