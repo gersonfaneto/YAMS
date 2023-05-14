8e9053d792b37e0f839b83080bb9922d00af7dc7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-14 20:58:51 -0300

    fix: Conflicts with JavaDoc and modules.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,4 +15,43 @@ module com.gersonfaneto.yams {
   opens com.gersonfaneto.yams.dao.stock;
 
   exports com.gersonfaneto.yams;
+
+  // FIX: Needed for making JavaDoc see the whole project. 
+  exports com.gersonfaneto.yams.dao;
+  exports com.gersonfaneto.yams.dao.billing.invoice;
+  exports com.gersonfaneto.yams.dao.billing.paymet;
+  exports com.gersonfaneto.yams.dao.entities.client;
+  exports com.gersonfaneto.yams.dao.entities.user;
+  exports com.gersonfaneto.yams.dao.orders.purchase;
+  exports com.gersonfaneto.yams.dao.orders.work;
+  exports com.gersonfaneto.yams.dao.services;
+  exports com.gersonfaneto.yams.dao.stock;
+
+  exports com.gersonfaneto.yams.exceptions.billing;
+  exports com.gersonfaneto.yams.exceptions.client;
+  exports com.gersonfaneto.yams.exceptions.orders;
+  exports com.gersonfaneto.yams.exceptions.services;
+  exports com.gersonfaneto.yams.exceptions.stock;
+  exports com.gersonfaneto.yams.exceptions.users;
+
+  exports com.gersonfaneto.yams.models.billing.invoice;
+  exports com.gersonfaneto.yams.models.billing.payments;
+  exports com.gersonfaneto.yams.models.entities.admnistrator;
+  exports com.gersonfaneto.yams.models.entities.client;
+  exports com.gersonfaneto.yams.models.entities.receptionist;
+  exports com.gersonfaneto.yams.models.entities.technician;
+  exports com.gersonfaneto.yams.models.entities.technician.states;
+  exports com.gersonfaneto.yams.models.entities.user;
+
+  exports com.gersonfaneto.yams.models.orders.work;
+  exports com.gersonfaneto.yams.models.orders.work.states;
+  exports com.gersonfaneto.yams.models.orders.purchase;
+  
+  exports com.gersonfaneto.yams.models.reports;
+
+  exports com.gersonfaneto.yams.models.services;
+
+  exports com.gersonfaneto.yams.models.stock;
+
+  exports com.gersonfaneto.yams.utils;
 }
