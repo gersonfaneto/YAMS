03c147a83d47a7fc10e6c3aa62c0619e63d94377
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-29 20:24:52 -0300

    feat(views): Added `appIcon`.
    Displayed on the menu bar.

--- src/main/java/com/gersonfaneto/yams/App.java
+++ src/main/java/com/gersonfaneto/yams/App.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -21,8 +22,12 @@ public class App extends Application {
 
     Scene loginScene = new Scene(root);
 
+    Image appIcon = new Image(getClass().getResourceAsStream("images/logo-no-background.png"));
+
     primaryStage.setResizable(false);
     primaryStage.initStyle(StageStyle.UNDECORATED);
+
+    primaryStage.getIcons().add(appIcon);
 
     primaryStage.setScene(loginScene);
     primaryStage.show();
