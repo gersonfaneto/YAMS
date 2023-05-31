251fc3a29dbe52a19536971fc944f24fdcddbe53
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-31 10:02:32 -0300

    feat(controllers): Add missing base code.

---
+++ src/main/java/com/gersonfaneto/yams/controllers/ServicesController.java
@@ -0,0 +1,16 @@
+package com.gersonfaneto.yams.controllers;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+
+public class ServicesController {
+  @FXML private FontAwesomeIconView closeButton;
+
+  @FXML
+  public void initialize() {}
+
+  @FXML
+  public void closeWindow() {
+    System.exit(0);
+  }
+}
