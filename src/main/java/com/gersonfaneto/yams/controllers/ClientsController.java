703eed0eadd035c22f03717b3260070d235bf169
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-31 09:57:48 -0300

    feat(controllers): Add basic controllers for the other panes.

--- src/main/java/com/gersonfaneto/yams/controllers/ClientsController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/ClientsController.java
@@ -3,14 +3,11 @@ package com.gersonfaneto.yams.controllers;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 
-public class HomeController {
+public class ClientsController {
+  @FXML private FontAwesomeIconView closeButton;
+
   @FXML
-  private FontAwesomeIconView closeButton;
-  
-  @FXML
-  public void initialize() {
-    System.out.println("Welcome, Home!");
-  }
+  public void initialize() {}
 
   @FXML
   public void closeWindow() {
--- src/main/java/com/gersonfaneto/yams/controllers/MenuController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/MenuController.java
@@ -8,16 +8,21 @@ import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 public class MenuController {
-  @FXML
-  private FontAwesomeIconView backButton;
+  @FXML private FontAwesomeIconView backButton;
+
+  @FXML private Button homeButton;
+  @FXML private Button clientsButton;
+  @FXML private Button servicesButton;
+  @FXML private Button stockButton;
+  @FXML private Button reportsButton;
 
   public static BorderPane mainWindow = null;
 
-  public void initialize() throws IOException {
-  }
+  public void initialize() {}
 
   @FXML
   public void resumeApp() throws IOException {
@@ -25,5 +30,40 @@ public class MenuController {
 
     mainWindow.getChildren().removeAll();
     mainWindow.getChildren().setAll(loginElements);
+  }
+
+  @FXML
+  public void openHomePane() throws IOException {
+    Parent homePaneElements = FXMLLoader.load(App.class.getResource("views/home.fxml"));
+
+    mainWindow.setRight(homePaneElements);
+  }
+
+  @FXML
+  public void openClientsPane() throws IOException {
+    Parent clientsPaneElements = FXMLLoader.load(App.class.getResource("views/clients.fxml"));
+
+    mainWindow.setRight(clientsPaneElements);
+  }
+
+  @FXML
+  public void openServicesPane() throws IOException {
+    Parent servicesPaneElements = FXMLLoader.load(App.class.getResource("views/services.fxml"));
+
+    mainWindow.setRight(servicesPaneElements);
+  }
+
+  @FXML
+  public void openStockPane() throws IOException {
+    Parent stockPaneElements = FXMLLoader.load(App.class.getResource("views/stock.fxml"));
+
+    mainWindow.setRight(stockPaneElements);
+  }
+
+  @FXML
+  public void openReportsPane() throws IOException {
+    Parent reporstPaneElements = FXMLLoader.load(App.class.getResource("views/reports.fxml"));
+
+    mainWindow.setRight(reporstPaneElements);
   }
 }
