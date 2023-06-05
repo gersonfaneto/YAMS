876359d85b0f46e18e38a7afc025977f7b1e9881
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-05 12:15:08 -0300

    feat(controllers/views): Add basic components to stock view.

--- src/main/java/com/gersonfaneto/yams/controllers/StockController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/StockController.java
@@ -1,13 +1,53 @@
 package com.gersonfaneto.yams.controllers;
 
+import com.gersonfaneto.yams.models.stock.Component;
+
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class StockController {
-  @FXML private FontAwesomeIconView closeButton;
+  @FXML
+  private FontAwesomeIconView closeButton;
 
   @FXML
-  public void initialize() {}
+  private TableView<Component> componentsTable;
+
+  @FXML
+  private TableColumn<Component, Integer> amountColumn;
+
+  @FXML
+  private TableColumn<Component, Double> costColumn;
+
+  @FXML
+  private TableColumn<Component, Double> priceColumn;
+
+  @FXML
+  private TableColumn<Component, String> descriptionColumn;
+
+  @FXML
+  private TextField searchField;
+
+  @FXML
+  private ComboBox<String> typeFilter;
+
+  @FXML
+  public void initialize() {
+    typeFilter.getItems().add("RAM");
+    typeFilter.getItems().add("Placa Mãe");
+    typeFilter.getItems().add("Fonte");
+    typeFilter.getItems().add("Placa de Vídeo");
+    typeFilter.getItems().add("HD/SSD");
+    typeFilter.getItems().add("Outros");
+  }
+
+  @FXML
+  void filterSearch() {
+
+  }
 
   @FXML
   public void closeWindow() {
