4b30a9254db32e7b945a1d0cc1b153244636073d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-05 15:10:47 -0300

    feat(views/controllers): Add base elements and actions.

--- src/main/java/com/gersonfaneto/yams/controllers/PurchaseComponentController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/PurchaseComponentController.java
@@ -1,6 +1,7 @@
 package com.gersonfaneto.yams.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.gersonfaneto.yams.App;
 
@@ -9,23 +10,80 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PurchaseComponentController {
-  @FXML
-  private FontAwesomeIconView backButton;
 
   @FXML
-  private Button cancelButton;
+  private FontAwesomeIconView backButton;
 
   @FXML
   private FontAwesomeIconView closeButton;
 
   @FXML
+  private Label visualFeedback;
+
+  @FXML
+  private TextField descriptionField;
+
+  @FXML
+  private ComboBox<String> typeSelector;
+
+  @FXML
+  private TextField typeField;
+
+  @FXML
+  private ComboBox<String> amountSelector;
+
+  @FXML
+  private TextField amountField;
+
+  @FXML
+  private TextField costField;
+
+  @FXML
+  private TextField priceField;
+
+  @FXML
+  private Button cancelButton;
+
+  @FXML
   private Button confirmButton;
+
 
   @FXML
   public void initialize() {
+    amountSelector.getItems().addAll(
+      List.of("5", "10", "15", "20", "Outro")
+    );
+    amountField.setVisible(false);
 
+    typeSelector.getItems().addAll(
+      List.of("RAM", "Placa Mãe", "Fonte", "Placa de Vídeo", "HD", "SSD", "Outro")
+    );
+    typeField.setVisible(false);
+  }
+
+  @FXML
+  public void checkAmount() {
+    if (amountSelector.getValue().equals("Outro")) {
+      amountField.setVisible(true);
+    }
+    else {
+      amountField.setVisible(false);
+    }
+  }
+
+  @FXML
+  public void checkType() {
+    if (typeSelector.getValue().equals("Outro")) {
+      typeField.setVisible(true);
+    }
+    else {
+      typeField.setVisible(false);
+    }
   }
 
   @FXML
@@ -36,7 +94,7 @@ public class PurchaseComponentController {
   }
 
   @FXML
-  void confirmRegister() {
+  void confirmPurchase() {
 
   }
 
@@ -46,4 +104,3 @@ public class PurchaseComponentController {
     System.exit(0);
   }
 }
-
