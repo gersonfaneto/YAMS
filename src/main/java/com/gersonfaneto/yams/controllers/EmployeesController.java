7797bfdffecfc1f1abf3a1811a2a7ef1d4ebf04d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-04 23:04:16 -0300

    feat(controllers): Populate table with models data.

--- src/main/java/com/gersonfaneto/yams/controllers/EmployeesController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/EmployeesController.java
@@ -1,17 +1,20 @@
 package com.gersonfaneto.yams.controllers;
 
+import com.gersonfaneto.yams.dao.DAO;
 import com.gersonfaneto.yams.models.entities.user.User;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EmployeesController {
 
   @FXML
-  private TableView<User> clientsTable;
+  private TableView<User> employeesTable;
 
   @FXML
   private FontAwesomeIconView closeButton;
@@ -23,13 +26,22 @@ public class EmployeesController {
   private TableColumn<User, String> nameColumn;
 
   @FXML
-  private TableColumn<User, String> statusColumn;
+  private TableColumn<User, String> roleColumn;
 
   @FXML
   private FontAwesomeIconView searchButton;
 
   @FXML
   private TextField searchField;
+
+  @FXML
+  public void initialize() {
+    emailColumn.setCellValueFactory(new PropertyValueFactory<>("userEmail"));
+    nameColumn.setCellValueFactory(new PropertyValueFactory<>("userName"));
+    roleColumn.setCellValueFactory(new PropertyValueFactory<>("userType"));
+
+    employeesTable.setItems(FXCollections.observableArrayList(DAO.fromUsers().findMany()));
+  }
 
   @FXML
   void closeWindow() {
