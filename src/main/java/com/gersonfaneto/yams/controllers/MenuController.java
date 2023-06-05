022b8ea59cd0f02e13ed7a544d5914e88ff8e7dc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-04 22:38:43 -0300

    feat(controllers): Unify function in a single `handleClicks`.

--- src/main/java/com/gersonfaneto/yams/controllers/MenuController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/MenuController.java
@@ -6,6 +6,7 @@ import com.gersonfaneto.yams.App;
 import com.gersonfaneto.yams.models.entities.user.UserType;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -57,52 +58,36 @@ public class MenuController {
   }
 
   @FXML
+  public void handleClicks(ActionEvent actionEvent) throws IOException {
+    Parent targetView = null;
+
+    if (actionEvent.getSource() == homeButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/home.fxml"));
+    }
+    else if (actionEvent.getSource() == clientsButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/clients.fxml"));
+    }
+    else if (actionEvent.getSource() == servicesButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/services.fxml"));
+    }
+    else if (actionEvent.getSource() == stockButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/stock.fxml"));
+    }
+    else if (actionEvent.getSource() == reportsButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/reports.fxml"));
+    }
+    else if (actionEvent.getSource() == employeesButton) {
+      targetView = FXMLLoader.load(App.class.getResource("views/employees.fxml"));
+    }
+
+    MainController.mainWindow.setRight(targetView);
+  }
+
+  @FXML
   public void signOut() throws IOException {
     Parent loginElements = FXMLLoader.load(App.class.getResource("views/login.fxml"));
 
     MainController.mainWindow.getChildren().removeAll();
     MainController.mainWindow.getChildren().setAll(loginElements);
-  }
-
-  @FXML
-  public void openHomePane() throws IOException {
-    Parent homePaneElements = FXMLLoader.load(App.class.getResource("views/home.fxml"));
-
-    MainController.mainWindow.setRight(homePaneElements);
-  }
-
-  @FXML
-  public void openClientsPane() throws IOException {
-    Parent clientsPaneElements = FXMLLoader.load(App.class.getResource("views/clients.fxml"));
-
-    MainController.mainWindow.setRight(clientsPaneElements);
-  }
-
-  @FXML
-  public void openServicesPane() throws IOException {
-    Parent servicesPaneElements = FXMLLoader.load(App.class.getResource("views/services.fxml"));
-
-    MainController.mainWindow.setRight(servicesPaneElements);
-  }
-
-  @FXML
-  public void openStockPane() throws IOException {
-    Parent stockPaneElements = FXMLLoader.load(App.class.getResource("views/stock.fxml"));
-
-    MainController.mainWindow.setRight(stockPaneElements);
-  }
-
-  @FXML
-  public void openReportsPane() throws IOException {
-    Parent reporstPaneElements = FXMLLoader.load(App.class.getResource("views/reports.fxml"));
-
-    MainController.mainWindow.setRight(reporstPaneElements);
-  }
-
-  @FXML
-  public void openEmployeesPane() throws IOException {
-    Parent employeesPaneElements = FXMLLoader.load(App.class.getResource("views/employees.fxml"));
-
-    MainController.mainWindow.setRight(employeesPaneElements);
   }
 }
