78997203a83539526ba28d0e96b576f2d7e3f7a3
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-15 21:31:20 -0300

    feat(controllers): Populate `listView` with related services.

--- src/main/java/com/gersonfaneto/yams/controllers/TechnicianOccupiedController.java
+++ src/main/java/com/gersonfaneto/yams/controllers/TechnicianOccupiedController.java
@@ -1,13 +1,20 @@
 package com.gersonfaneto.yams.controllers;
 
+import java.util.List;
+
 import com.gersonfaneto.yams.dao.DAO;
 import com.gersonfaneto.yams.models.entities.technician.Technician;
 import com.gersonfaneto.yams.models.orders.work.WorkOrder;
 import com.gersonfaneto.yams.models.services.Service;
 import com.gersonfaneto.yams.utils.Time;
+import com.gersonfaneto.yams.views.components.ComponentSize;
+import com.gersonfaneto.yams.views.components.ServicesListComponent;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -29,6 +36,8 @@ public class TechnicianOccupiedController {
 
   @FXML
   private Button finishOrderButton;
+
+  private ObservableList<Service> servicesList;
 
   private Technician loggedTechnician;
   private WorkOrder openOrder;
@@ -57,6 +66,35 @@ public class TechnicianOccupiedController {
             .reduce(0.0, Double::sum)
         )
     );
+
+    servicesList = FXCollections.observableArrayList();
+
+    listView.setCellFactory(listView -> new ListCell<Service>() {
+      @Override
+      protected void updateItem(Service service, boolean empty) {
+        super.updateItem(service, empty);
+
+        if (service == null || empty) {
+          setGraphic(null);
+        } else {
+          ServicesListComponent clientComponent = new ServicesListComponent(
+              service,
+              servicesList,
+              ComponentSize.Small,
+              true
+          );
+
+          setGraphic(clientComponent);
+        }
+      }
+    });
+
+    List<Service> relatedServices = DAO.fromService()
+      .findByWorkOrder(openOrder.getWorkOrderID());
+
+    servicesList.addAll(relatedServices);
+
+    listView.setItems(servicesList);
   }
 
   @FXML
